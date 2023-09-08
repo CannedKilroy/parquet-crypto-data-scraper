68480405f79ced808156169fdc08e1c9102ab760
CompSci_Enthusiast <112599362+CannedKilroy@users.noreply.github.com>
2023-09-08 10:14:26 -0600

    Update main.py
    Made writing to database async and removed redundancy

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import datetime
 from storage import meta, table_ohlcv, table_orderbook, table_trades, table_ticker, table_logs
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite:///data.db', echo = False)
-meta.create_all(engine)
-
 #symbols
 btc_inverse_perp = 'BTC/USD:BTC'
 btc_linear_perp = 'BTC/USDT:USDT'
@@ -30,7 +27,7 @@ if sys.version_info < (3,7):
     sys.exit(1)
 print('CCXT version', ccxt.pro.__version__)
 
-async def watch_order_book(exchange, symbol, orderbook_depth):
+async def watch_order_book(exchange, symbol, orderbook_depth, engine):
     '''
     Watch the order book for a specific symbol.
     Orderbook is unique from api, updates on deltas
@@ -38,31 +35,33 @@ async def watch_order_book(exchange, symbol, orderbook_depth):
     :param exchange: The exchange object
     :param symbol: The trading symbol
     :param orderbook_depth: The depth of the order book
+    :param engine: SQLalchemy engine
     '''
-    last_orderbook = None
     name = getattr(exchange, 'name')
     
     while True:
         try:
             orderbook = await exchange.watch_order_book(symbol, orderbook_depth)
-            with engine.connect() as conn:
-                command = table_orderbook.insert().values(
-                    exchange = name,
-                    symbol = orderbook['symbol'],
-                    asks = orderbook['asks'],
-                    bids = orderbook['bids'],
-                    nonce = orderbook['nonce'],
-                    datetime = datetime.datetime.fromisoformat(orderbook['datetime']),
-                    created_at = orderbook['timestamp']
+            conn = await engine.connect()
+
+            async with conn.begin():
+                await conn.execute(
+                    table_orderbook.insert().values(
+                        exchange = name,
+                        symbol = orderbook['symbol'],
+                        asks = orderbook['asks'],
+                        bids = orderbook['bids'],
+                        nonce = orderbook['nonce'],
+                        datetime = datetime.datetime.fromisoformat(orderbook['datetime']),
+                        created_at = orderbook['timestamp']
                     )
-                conn.execute(command)
-                conn.commit()
+            await conn.close()
         
         except Exception as e:
             print(str(e))
             raise e
     
-async def watch_trades(exchange, symbol):
+async def watch_trades(exchange, symbol, engine):
     '''
     Watch the trades for a specific symbol.
     Trades are unique from the api
@@ -70,39 +69,39 @@ async def watch_trades(exchange, symbol):
     :param exchange: The exchange object
     :param symbol: The trading symbol
     '''
-    table_created = None
     name = getattr(exchange, 'name')
     
     while True:
         try:
             trades = await exchange.watch_trades(symbol)
-            with engine.connect() as conn:
-                for trade in trades:
-                    command = table_trades.insert().values(
-                    exchange = name,
-                    symbol = symbol,
-                    trade_id = trade['id'],
-                    order_id = trade['order'],
-                    order_type = trade['type'],
-                    trade_side = trade['side'],
-                    taker_maker = trade['takerOrMaker'],
-                    executed_price = trade['price'],
-                    base_amount = trade['amount'],
-                    cost = trade['cost'],
-                    fee = trade['fee'],
-                    fees = trade['fees'] if trade['fees'] else None,
-                    datetime = datetime.datetime.fromisoformat(trade['datetime']),
-                    created_at = trade['timestamp']
+            
+            conn = await engine.connect()
+            for trade in trades:
+                async with conn.begin():
+                    await conn.execute(
+                        table_trades.insert().values(
+                            exchange = name,
+                            symbol = symbol,
+                            trade_id = trade['id'],
+                            order_id = trade['order'],
+                            order_type = trade['type'],
+                            trade_side = trade['side'],
+                            taker_maker = trade['takerOrMaker'],
+                            executed_price = trade['price'],
+                            base_amount = trade['amount'],
+                            cost = trade['cost'],
+                            fee = trade['fee'],
+                            fees = trade['fees'] if trade['fees'] else None,
+                            datetime = datetime.datetime.fromisoformat(trade['datetime']),
+                            created_at = trade['timestamp'])
                     )
-                    
-                    conn.execute(command)
-                    conn.commit()
+            await conn.close()
                     
         except Exception as e:
             print(str(e))
             raise e
 
-async def watch_ohlcv(exchange, symbol, timeframe, candle_limit):
+async def watch_ohlcv(exchange, symbol, timeframe, candle_limit, engine):
     '''
     Watch the OHLCV data for a specific symbol.
 
@@ -117,6 +116,7 @@ async def watch_ohlcv(exchange, symbol, timeframe, candle_limit):
     while True:
         try:
             candle = await exchange.watch_ohlcv(symbol, timeframe, None, candle_limit)
+            conn = await engine.connect()
             
             if last_candle is None:
                 last_candle = candle
@@ -126,28 +126,27 @@ async def watch_ohlcv(exchange, symbol, timeframe, candle_limit):
                 datetime_str = exchange.iso8601(last_candle[0][0])
                 datetime_obj = datetime.datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S.%fZ')
 
-                with engine.connect() as conn:
-                    command = table_ohlcv.insert().values(
-                        exchange = name,
-                        symbol = symbol,
-                        open_price = last_candle[0][1],
-                        high_price = last_candle[0][2],
-                        low_price = last_candle[0][3],
-                        close_price = last_candle[0][4],
-                        candle_volume = last_candle[0][5],
-                        created_at = last_candle[0][0],
-                        datetime = datetime.datetime.utcfromtimestamp(last_candle[0][0]/1000)
+                async with conn.begin():
+                    await conn.execute(
+                        table_ohlcv.insert().values(
+                            exchange = name,
+                            symbol = symbol,
+                            open_price = last_candle[0][1],
+                            high_price = last_candle[0][2],
+                            low_price = last_candle[0][3],
+                            close_price = last_candle[0][4],
+                            candle_volume = last_candle[0][5],
+                            created_at = last_candle[0][0],
+                            datetime = datetime.datetime.utcfromtimestamp(last_candle[0][0]/1000))
                         )
-                
-                    conn.execute(command)
-                    conn.commit()
+            await conn.close()
             last_candle = candle
-            
+
         except Exception as e:
             print(str(e))
             raise e
             
-async def watch_ticker(exchange, symbol):
+async def watch_ticker(exchange, symbol, engine):
     '''
     Watch the ticker of an exchange for a specific symbol.
 
@@ -155,66 +154,51 @@ async def watch_ticker(exchange, symbol):
     :param symbol: The trading symbol
     '''
     name = getattr(exchange, 'name')
-    last_ticker = None
         
-    while True:
+   while True:
         try:
             ticker = await exchange.watch_ticker(symbol)
             
-            command = table_ticker.insert().values(
-                exchange = name,
-                symbol = symbol,
-                ask = ticker['ask'],
-                ask_volume = ticker['askVolume'],
-                bid = ticker['bid'],
-                bid_volume = ticker['bidVolume'],
-                open_24h = ticker['open'],
-                high_24h = ticker['high'],
-                low_24h = ticker['low'],
-                close_24h = ticker['close'],
-                last_price = ticker['last'],
-                vwap = ticker['vwap'],
-                previous_close_price = ticker['previousClose'],
-                price_change = ticker['change'],
-                percentage_change = ticker['percentage'],
-                average_price = ticker['average'],
-                base_volume = ticker['baseVolume'],
-                quote_volume = ticker['quoteVolume'],
-                info = ticker['info'],
-                datetime = datetime.datetime.fromisoformat(ticker['datetime']),
-                created_at = ticker['timestamp']
-                )
-            
-            #create timeless dict to check for uniqueness
-            unique_ticker = ticker.copy()
-            removal = ['timestamp', 'info', 'datetime']
-            for key in removal:
-                unique_ticker.pop(key)
-
-            if last_ticker is None:
-                last_ticker = [ticker, unique_ticker]                
-                with engine.connect() as conn:
-                    conn.execute(command)
-                    conn.commit()
-                    
-            if unique_ticker != last_ticker[1]:
-                
-                #if they are not equal insert new ticker
-                with engine.connect() as conn:
-                    conn.execute(command)
-                    conn.commit()
-                    
-                                
-                #set last ticker to new one
-                last_ticker[0] = ticker
-                last_ticker[1] = unique_ticker
+            conn = await engine.connect()
+            async with conn.begin():
+                await conn.execute(
+                    table_ticker.insert().values(
+                        exchange = name,
+                        symbol = symbol,
+                        ask = ticker['ask'],
+                        ask_volume = ticker['askVolume'],
+                        bid = ticker['bid'],
+                        bid_volume = ticker['bidVolume'],
+                        open_24h = ticker['open'],
+                        high_24h = ticker['high'],
+                        low_24h = ticker['low'],
+                        close_24h = ticker['close'],
+                        last_price = ticker['last'],
+                        vwap = ticker['vwap'],
+                        previous_close_price = ticker['previousClose'],
+                        price_change = ticker['change'],
+                        percentage_change = ticker['percentage'],
+                        average_price = ticker['average'],
+                        base_volume = ticker['baseVolume'],
+                        quote_volume = ticker['quoteVolume'],
+                        info = ticker['info'],
+                        datetime = datetime.datetime.fromisoformat(ticker['datetime']),
+                        created_at = ticker['timestamp'])
+                    )
+            await conn.close()
             
         except Exception as e:
             print(str(e))
             raise e
 
-async def main():    
-    exchange = ccxt.pro.bybit({'newUpdates':True,'enableRateLimit': True, 'verbose':False})
+async def main():
+    engine = create_async_engine(
+        'sqlite+aiosqlite:///dataaa.db', 
+        echo = True)
+    async with engine.begin() as conn:
+        await conn.run_sync(meta.create_all)
+    
+    exchange = ccxt.pro.bybit({'newUpdates':True,'enableRateLimit': True, 'verbose':True})
     
     await exchange.load_markets()        
     symbol = btc_inverse_perp
@@ -222,16 +206,16 @@ async def main():
     loops = []
     if exchange.has["watchOHLCV"]:
         loops.append(
-            watch_ohlcv(exchange, symbol, timeframe, candle_limit))
+            watch_ohlcv(exchange, symbol, timeframe, candle_limit, engine))
     if exchange.has["watchTicker"]:
         loops.append(
-            watch_ticker(exchange, symbol))
+            watch_ticker(exchange, symbol, engine))
     if exchange.has["watchTrades"]:
         loops.append(
-            watch_trades(exchange, symbol))
+            watch_trades(exchange, symbol, engine))
     if exchange.has["watchOrderBook"]:
         loops.append(
-            watch_order_book(exchange, symbol, orderbook_depth))
+            watch_order_book(exchange, symbol, orderbook_depth, engine))
     
     while True:
         await asyncio.gather(*loops)
